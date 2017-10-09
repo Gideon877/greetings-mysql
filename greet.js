f2f7be33863057cc528825f585d370865263cc8e
Thabang Gideon Magaola <thabang@projectcodex.co>
2017-10-09 16:11:15 +0200

    Initial commit

--- greet.js
+++ greet.js
@@ -1,171 +1,114 @@
-module.exports = function(models) {
+exports.index = function(req, res, done) {
+    var input = req.body;
+    req.getConnection(function(err, connection) {
+        if (err) return done(err);
+        connection.query('SELECT * from greetings WHERE name=?', input.name, function(err, results) {
+            // if (err) return done(err);
+            var result = results[0];
+            var language = input.language;
 
-    const index = function(req, res, done) {
-        var outputGreeting = "";
-        var language = req.body.language;
-        var counterMsg = "";
+            //Exist
+            if (result) {
+                var new_count = result.count + 1;
+                var id = result.id
 
+                connection.query("UPDATE greetings set count=? WHERE id=?", [new_count, id],
+                    function(err, rows) {
+                        if (err)
+                            console.log("Error Updating : %s ", err);
 
+                        connection.query('SELECT * from greetings',
+                            function(err, Names) {
+                                var data = {
+                                    greeting: language + result.name,
+                                    counts: "Names greeted for this session: " + Names.length
+                                };
 
-        var nameData = {
-            name: req.body.name
-        }
-        if (!nameData || !nameData.name) {
-            req.flash('error', 'Name should not be blank');
-            res.render('index')
-        } else {
-            if (!language) {
-                req.flash("error", "Please select language");
-                res.render('index');
-            } else {
-                models.Name.findOne({
-                    name: req.body.name
-                }, function(err, theName) {
-
-                    if (err) {
-                        return done(err)
-                    }
-
-                    var otherName = req.body.name;
-                    var myName = otherName.toLowerCase();
-
-                    function capitalizeFirstLetter(string) {
-                        return string.charAt(0).toUpperCase() + string.slice(1);
-                    }
-                    var name = capitalizeFirstLetter(myName)
-                    console.log(name);
-
-                    if (theName) {
-                        theName.greetCounter = theName.greetCounter + 1;
-
-                        if (language == 'english') {
-                            outputGreeting = "Hello, " + theName.name;
-                        } else if (language == 'setswana') {
-                            outputGreeting = "Dumela, " + theName.name;
-                        } else if (language == 'zulu') {
-                            outputGreeting = "Sawubona, " + theName.name;
-                        }
-
-                        theName
-                            .save(function(err, result) {
-                                if (err) {
-                                    return done(err)
-                                }
+                                res.render('index', data);
                             });
 
-                        models.Name.find({},  function(err,  result) { 
-                            if (err) {
-                                done(err)
-                            } 
-                            var counterNmbr = result.length;
-
-                            counterMsg = "Names greeted for this session: " + result.length;
-                            var data = {
-                                greeting: outputGreeting,
-                                counts: counterMsg
-                            };
-
-                            res.render('index', data);
-                        });
-                    };
-
-                    if (!theName) {
-
-
-
-                        models.Name.create({
-                            name: name,
-                            greetCounter: 1
-                        }, function(err, result) {
-                            if (err) {
-                                return done(err);
-                            }
-                            models.Name.find({
-                                name: name
-                            }, function(err, results) {
-                                if (err) {
-                                    return done(err);
-                                }
-
-                                if (language == 'english') {
-                                    outputGreeting = "Hello, " + result.name;
-                                } else if (language == 'setswana') {
-                                    outputGreeting = "Dumela, " + result.name;
-                                } else if (language == 'zulu') {
-                                    outputGreeting = "Sawubona, " + result.name;
-                                }
-                                models.Name.find({},  function(err,  result) {
-                                    if (err) {
-                                        done(err)
-                                    };
-
-                                    counterMsg = "Names greeted for this session: " + result.length;
-                                    var data = {
-                                        greeting: outputGreeting,
-                                        counts: counterMsg
-                                    };
-                                    res.render('index', data);
-                                });
-                            });
-                        });
-                    };
-                });
-            };
-        };
-    };
-
-    const greeted = function(req, res, done) {
-
-        models.Name.find({},  function(err,  result) {
-
-            if (err) {
-                return done(err);
+                    });
             }
+            //No name in database
+            if (!result) {
+                var newuser = {
+                    name: input.name,
+                    count: 1
+                }
+                connection.query("INSERT INTO greetings set ? ", newuser,
+                    function(err, rows) {
+                        if (err)
+                            console.log("Error Updating : %s ", err);
 
-            console.log('Greeted', result);
+                        connection.query('SELECT * from greetings',
+                            function(err, Names) {
+                                var data = {
+                                    greeting: language + input.name,
+                                    counts: "Names greeted for this session: " + Names.length
+                                };
+
+                                res.render('index', data);
+                            });
+
+                    });
+            }
+        })
+
+    });
+};
+
+exports.greeted = function(req, res, done) {
+    req.getConnection(function(err, connection) {
+        if (err) return done(err);
+        connection.query('SELECT * from greetings', [], function(err, results) {
+            if (err) return done(err);
 
             var data_2 = {
-                namesGreeted: result
+                namesGreeted: results
             };
             res.render('greeted', data_2);
         });
-    };
+    });
+};
 
-    const counter = function(req, res, done) {
+exports.counter = function(req, res, done) {
+    var user_id = req.params.user_id;
 
-        var user_id = req.params.user_id;
+    req.getConnection(function(err, connection) {
+        connection.query("SELECT * from greetings WHERE id = ?", [user_id], function(err, rows) {
+            if (err)
+                console.log("Error deleting : %s ", err);
 
-        models.Name.findOne({
-            _id: user_id
-        }, function(err, result) {
-
-            if (err) {
-                done(err)
-            } 
-            var output = result.name + " have been greeted " + result.greetCounter + ' time(s).';
+            var output = rows[0].name + " have been greeted " + rows[0].count + ' time(s).';
 
             var data_3 = {
                 countMsg: output
-            }
+            };
             res.render('counter', data_3);
         });
-    }
+    });
 
-    const clearHistory = function(req, res, done) {
-        models.Name.remove({}, function(err, result) {
-            if (err) {
-                done(err)
-            }
+};
+
+exports.clearHistory = function(req, res, done) {
+    req.getConnection(function(err, connection) {
+        connection.query("DELETE FROM greetings", function(err, result) {
+            if (err)
+                console.log("Error deleting : %s ", err);
+
+
+
+            connection.query("SET @idnum := 0;", function(err, result) {
+                connection.query("UPDATE greetings SET id = @idnum := (@idnum+1)", function(err, result) {
+                    connection.query("ALTER TABLE greetings AUTO_INCREMENT = 1", function(err, result) {
+
+                    });
+                });
+            });
+
+
 
             res.render('index');
-
         });
-    }
-
-    return {
-        index,
-        greeted,
-        counter,
-        clearHistory
-    };
+    });
 };
--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const flash = require('express-flash');
-const session = require('express-session');
+'use strict'
+var express = require('express'),
+    flash = require('express-flash'),
+    session = require('express-session'),
+    exphbs  = require('express-handlebars'),
+    mysql = require('mysql'),
+    myConnection = require('express-myconnection'),
+    bodyParser = require('body-parser'),
+    routes = require('./greet');
 
 
-const NameRoutes = require('./greet');
-const Models = require('./models');
-const models = Models(process.env.MONGO_DB_URL || 'mongodb://localhost/greetings');
-const nameRoutes = NameRoutes(models);
 const app = express();
 
-app.set("port", (process.env.PORT || 3002))
+var dbOptions = {
+      host: 'localhost',
+      user: 'root',
+      password: '1',
+      port: 3306,
+      database: 'greetings'
+};
+
+app.set("port", (process.env.PORT || 3002));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
+//setup middleware
+app.use(myConnection(mysql, dbOptions, 'single'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -27,17 +37,23 @@ app.use(bodyParser.json())
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 * 30}}));
 app.use(flash()); // set up http session
 
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
 
-app.get('/', nameRoutes.index);
-app.get('/greeted', nameRoutes.greeted);
-app.get('/counter/:user_id', nameRoutes.counter);
-app.get('/clear', nameRoutes.clearHistory);
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/greeted', routes.greeted);
+app.get('/counter/:user_id', routes.counter);
+app.get('/clear', routes.clearHistory);
 
-app.post('/', nameRoutes.index);
-app.post('/clear', nameRoutes.clearHistory);
+app.post('/', routes.index);
+// app.post('/clear', routes.clearHistory);
 
-
-var port = app.get("port");
+app.use(errorHandler);
+var port = process.env.GREET_PORT_NR || 3000;
 
 app.listen(port, function() {
     console.log('App started on port: ' + port)
